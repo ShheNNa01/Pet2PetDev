93c4b831cdf7ae356212b1364eec8b6968e91e0e
cjotasM <107132483+cjotasM@users.noreply.github.com>
2024-11-21 09:27:11 -0500

    adding deployment apart

--- front/p2pFront/vite.config.js
+++ front/p2pFront/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -15,5 +14,16 @@ export default defineConfig({
   },
   define: {
     'process.env': {}
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   }
 })
